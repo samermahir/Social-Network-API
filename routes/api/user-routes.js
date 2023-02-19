d4a13545c32d8a9fe4c220ec8749828cd695c5ef
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-02-19 14:24:49 -0800

    user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -77,11 +77,36 @@ router.delete('/:userId', (req,res)=> {
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
 router.put('/:userId/friends/:friendId', (req,res)=> {
-
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: { friends: req.params.friendId} },
+        { runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res.status(404).json({ message: 'No user with this id found!' })
+            : res.json(user)
+        )
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 })
 
 //TODO - ROUTE THAT DELETES A FRIEND FROM A USER'S FRIENDS, DONT DELETE THE FRIEND AS A USER THOUGH!
 router.delete('/:userId/friends/:friendId', (req,res)=> {
+    User.findOneAndUpdate(
+
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } }, 
+            { runValidators: true, new: true }
+          )
+            .then((user) =>
+              !user
+                ? res.status(404).json({ message: 'No user with this id!' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
   
 });
 
